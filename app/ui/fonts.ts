c5e57b29c582b869ec6383791bb2f60fcc8769e2
Hamed Nazari <hmdnazari76@gmail.com>
2023-12-07 18:15:08 +0330

    s1

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], weight: '400' }); // You can change '400' to the desired weight
+export const lustina = Lusitana({ subsets: ['latin'], weight: '400' }); // You can change '400' to the desired weight
