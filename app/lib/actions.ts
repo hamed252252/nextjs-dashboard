9225120ee9f129a09f4dbe426607fa04ada7d987
Hamed Nazari <hmdnazari76@gmail.com>
2023-12-11 16:05:51 +0330

    s5

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,19 +5,38 @@ import { z } from 'zod';
 import { redirect } from 'next/navigation';
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({ invalid_type_error: 'Please select a customer.' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   // sql Insertaion
@@ -32,10 +51,10 @@ export async function createInvoice(formData: FormData) {
     };
   }
 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
   console.log(amountInCents);
   console.log(customerId, '  ', amount, +'    ', status);
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
   // console.log(rawFormData);
   // console.log(typeof rawFormData.amount);
 }
